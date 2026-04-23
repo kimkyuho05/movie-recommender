de0b3d13bfdbdfd17dfe88c1cee8ac48f35435e9
kimkyuho05 <kyuho05@soongsil.ac.kr>
2026-04-23 15:33:22 +0900

    진짜 최종

--- include/MovieManager.h
+++ include/MovieManager.h
@@ -1,19 +1,6 @@
 #pragma once
 #include <vector>
 #include <string>
-#include "User.h"
-
-class UserManager {
-private:
-    std::vector<User> users;
-
-public:
-    void addUser(const User& u);
-    void printAll() const;
-    User* findById(int id);
-};#pragma once
-#include <vector>
-#include <string>
 #include "Movie.h"
 
 class MovieManager {
--- include/User.h
+++ include/User.h
@@ -1,18 +1,5 @@
 #pragma once
 #include <string>
-
-class User {
-private:
-    int id;
-    std::string name;
-    std::string email;
-
-public:
-    User(int id, std::string name, std::string email);
-    int getId() const;
-    void display() const;
-};#pragma once
-#include <string>
 #include <iostream>
 
 class User {
