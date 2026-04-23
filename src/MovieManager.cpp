8b7ce1351cbb353d5ddfd6d478f62d08ba2da6fb
kimkyuho05 <kyuho05@soongsil.ac.kr>
2026-04-23 11:52:27 +0900

    movie.cpp

---
+++ src/MovieManager.cpp
@@ -0,0 +1,29 @@
+#include "MovieManager.h"
+#include <iostream>
+#include <algorithm>
+
+void MovieManager::addMovie(Movie m) {
+    movies.push_back(m);
+}
+
+void MovieManager::printAll() const {
+    if (movies.empty()) {
+        return;
+    }
+    for (int i = 0; i < (int)movies.size(); i++) {
+        movies[i].display();
+    }
+}
+
+Movie* MovieManager::findByTitle(std::string title) {
+    for (int i = 0; i < (int)movies.size(); i++) {
+        if (movies[i].getTitle() == title) {
+            return &movies[i];
+        }
+    }
+    return nullptr;
+}
+
+void MovieManager::sortByRating() {
+    std::sort(movies.begin(), movies.end());
+}
